07d20803209cba223823d5676c1a6cdb9ade08d4
Billy Wong <bwong.yh@protonmail.com>
2022-03-24 05:42:50 +0900

    edited with eslint

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,6 @@
 const assertEqual = (actual, expected) => {
-  const errorEmoji = "\u274C"
-  const successEmoji = "\uD83D\uDFE2"
+  const errorEmoji = "\u274C";
+  const successEmoji = "\uD83D\uDFE2";
   let output;
 
   actual === expected ? output = `${successEmoji} Assertion Passed: ${actual} === ${expected}` : output = `${errorEmoji} Assertion Failed: ${actual} !== ${expected}`;
