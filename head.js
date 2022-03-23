679fd0902cace18eea32fdb1919a47bcc7f0d8ee
Billy Wong <bwong.yh@protonmail.com>
2022-03-24 06:07:25 +0900

    tested with a few more scenarios & edited with eslint

--- head.js
+++ head.js
@@ -1,14 +1,19 @@
 const assertEqual = (actual, expected) => {
-  const errorEmoji = "\u274C";
-  const successEmoji = "\uD83D\uDFE2";
+  const errorEmoji = '\u274C';
+  const successEmoji = '\uD83D\uDFE2';
   let output;
 
-  actual === expected ? output = `${successEmoji} Assertion Passed: ${actual} === ${expected}` : output = `${errorEmoji} Assertion Failed: ${actual} !== ${expected}`;
-  
+  actual === expected
+    ? (output = `${successEmoji} Assertion Passed: ${actual} === ${expected}`)
+    : (output = `${errorEmoji} Assertion Failed: ${actual} !== ${expected}`);
+
   console.log(output);
 };
 
-const head = (arr) => arr[0]
+const head = (arr) => arr[0];
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([ 5, 6, 7 ]), 5);
+assertEqual(head([ 'Hello', 'Lighthouse', 'Labs' ]), 'Hello');
+assertEqual(head([ false ]), false);
+assertEqual(head([ true ]), false);
+assertEqual(head([]), 'Hello');
