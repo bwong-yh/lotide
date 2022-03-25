4a5b754f3e603366a7128301d5ac7568b9560e6d
Billy Wong <bwong.yh@protonmail.com>
2022-03-25 21:32:05 +0900

    created flatten.js & included flatten function within

---
+++ flatten.js
@@ -0,0 +1,33 @@
+const eqArrays = (arr1, arr2) => {
+  let result;
+
+  for (let i = 0; i < arr1.length; i++) {
+    arr1[i] === arr2[i] ? (result = true) : (result = false);
+  }
+
+  return result;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  const errorEmoji = '\u274C';
+  const successEmoji = '\uD83D\uDFE2';
+  let output;
+
+  eqArrays(actual, expected)
+    ? (output = `${successEmoji} Assertion Passed: ${actual} === ${expected}`)
+    : (output = `${errorEmoji} Assertion Failed: ${actual} !== ${expected}`);
+
+  console.log(output);
+};
+
+const flatten = (nestedArr) => {
+  const flattenedArr = []
+
+  for (item of nestedArr) {
+    !Array.isArray(item) ? flattenedArr.push(item) : flattenedArr.push(...item)
+  }
+
+  console.log(flattenedArr)
+}
+
+flatten([1, 2, [3, 4], 5, [6]])
