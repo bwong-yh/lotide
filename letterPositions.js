6b20de8fb4f3da20413d1d6da63ba0c3738d8a66
Billy Wong <bwong.yh@protonmail.com>
2022-03-29 03:21:23 +0900

    updated conditional statement to ternary operator, implemented with eqArrays and assertArraysEqual function to compare arrays

--- letterPositions.js
+++ letterPositions.js
@@ -1,16 +1,41 @@
+const eqArrays = (arr1, arr2) => {
+  let result;
+
+  if (arr1.length === 0 && arr1.length === arr2.length) {
+    return true;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      arr1[i] === arr2[i] ? (result = true) : (result = false);
+    }
+  }
+
+  return result;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  const errorEmoji = "\u274C";
+  const successEmoji = "\uD83D\uDFE2";
+  let output;
+
+  eqArrays(actual, expected)
+    ? (output = `${successEmoji} Assertion Passed: [${actual}] === [${expected}]`)
+    : (output = `${errorEmoji} Assertion Failed: [${actual}] !== [${expected}]`);
+
+  console.log(output);
+};
+
 const letterPositions = sentence => {
   const results = {};
   const sentenceLetters = sentence.split(" ").join("");
 
   for (let i = 0; i < sentenceLetters.length; i++) {
-    if (results[sentenceLetters[i]]) {
-      results[sentenceLetters[i]].push(i);
-    } else {
-      results[sentenceLetters[i]] = [i];
-    }
+    results[sentenceLetters[i]]
+      ? results[sentenceLetters[i]].push(i)
+      : (results[sentenceLetters[i]] = [i]);
   }
+
   console.log(results);
   return results;
 };
 
-letterPositions("lighthouse in the house");
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
