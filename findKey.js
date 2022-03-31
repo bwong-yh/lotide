033c3c490440d26fca41dd05d114834fe7cdef8d
Billy Wong <bwong.yh@protonmail.com>
2022-04-01 03:46:49 +0900

    created findKey.js & findKey function

---
+++ findKey.js
@@ -0,0 +1,26 @@
+const assertEqual = (actual, expected) => {
+  const errorEmoji = "\u274C";
+  const successEmoji = "\uD83D\uDFE2";
+  let output;
+  
+  actual === expected ? output = `${successEmoji} Assertion Passed: ${actual} === ${expected}` : output = `${errorEmoji} Assertion Failed: ${actual} !== ${expected}`;
+    
+  console.log(output);
+};
+
+const findKey = (obj, callback) => {
+  for (const [key, value] of Object.entries(obj)) {
+    if (callback(value)) return key;
+  }
+
+  return undefined;
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 10), undefined);
