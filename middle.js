e7c34c77e91ccbcfd91364e81a83105ffb6a9382
Billy Wong <bwong.yh@protonmail.com>
2022-04-13 04:37:07 +0900

    Update minor refactoring: will return the array directly instead of storing array to a variable and return it

--- middle.js
+++ middle.js
@@ -2,17 +2,14 @@ const assertArraysEqual = require("./assertArraysEqual");
 
 const middle = arr => {
   const middleIdx = Math.floor(arr.length / 2); // for odd number of length
-  const middleArr = [];
 
-  if (arr.length < 3) return middleArr;
+  if (arr.length < 3) return [];
 
   if (arr.length % 2 !== 0) {
-    middleArr.push(arr[middleIdx]);
+    return [arr[middleIdx]];
   } else {
-    middleArr.push(arr[middleIdx - 1], arr[middleIdx]);
+    return [arr[middleIdx - 1], arr[middleIdx]];
   }
-
-  return middleArr;
 };
 
 module.exports = { assertArraysEqual, middle };
