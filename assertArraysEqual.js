7c5acd42c4ab6783e97d7dba1096b319a054187b
Billy Wong <bwong.yh@protonmail.com>
2022-04-13 04:08:25 +0900

    Update refactored assertArraysEqual.js & created assertArraysEqualTest.js for test code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,25 +1,15 @@
-const eqArrays = (arr1, arr2) => {
-  let result;
-
-  for (let i = 0; i < arr1.length; i++) {
-    arr1[i] === arr2[i] ? (result = true) : (result = false);
-  }
-
-  return result;
-};
+const { eqArrays } = require("./eqArrays");
 
 const assertArraysEqual = (actual, expected) => {
-  const errorEmoji = '\u274C';
-  const successEmoji = '\uD83D\uDFE2';
+  const errorEmoji = "\u274C";
+  const successEmoji = "\uD83D\uDFE2";
   let output;
 
   eqArrays(actual, expected)
-    ? (output = `${successEmoji} Assertion Passed: ${actual} === ${expected}`)
-    : (output = `${errorEmoji} Assertion Failed: ${actual} !== ${expected}`);
+    ? (output = `${successEmoji} Assertion Passed: [${actual}] === [${expected}]`)
+    : (output = `${errorEmoji} Assertion Failed: [${actual}] !== [${expected}]`);
 
   console.log(output);
 };
 
-assertArraysEqual([ 1, 2, 3 ], [ 1, 2, '3' ]);
-assertArraysEqual([ 1, 2, 3 ], [ 1, 2, 3 ]);
-assertArraysEqual([ 1, 2, 3 ], [ 4, 5, 6 ]);
+module.exports = assertArraysEqual;
