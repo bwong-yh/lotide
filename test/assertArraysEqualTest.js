de5f716058f6e283e7dd220cc975862c2b2f66f3
Billy Wong <bwong.yh@protonmail.com>
2022-04-14 05:00:44 +0900

    Update commented out uncessary code for auto test

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,5 @@
 const assertArraysEqual = require("../assertArraysEqual");
 
-assertArraysEqual([1, 2, 3], [1, 2, "3"]);
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [4, 5, 6]);
+// assertArraysEqual([1, 2, 3], [1, 2, "3"]);
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [4, 5, 6]);
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,9 +1,9 @@
 const assertEqual = require("../assertEqual");
 
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 3);
-assertEqual(1, "1");
-assertEqual(true, true);
-assertEqual(true, false);
+// assertEqual("Lighthouse Labs", "Lighthouse Labs");
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual(1, 3);
+// assertEqual(1, "1");
+// assertEqual(true, true);
+// assertEqual(true, false);
