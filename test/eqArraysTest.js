f4f3cb1c47a3790b467add0dfd588c54a1f1acf0
Billy Wong <bwong.yh@protonmail.com>
2022-04-14 04:27:01 +0900

    Update line 6 changed for auto test

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,13 +1,9 @@
 const assert = require("chai").assert;
 const eqArrays = require("../eqArrays");
 
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
-// assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true);
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-
 describe("#eqArrays", () => {
   it("returns [1, 2, 3] for [1, 2, 3]", () => {
-    const result = eqArrays([1, 2, 3], [1, 2, 3, 4]);
+    const result = eqArrays([1, 2, 3], [1, 2, 3]);
     assert.strictEqual(result, true);
   });
 
