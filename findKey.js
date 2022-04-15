d1584bf21dba6bdc282500aad2620be6b1d1bec2
Billy Wong <bwong.yh@protonmail.com>
2022-04-15 19:39:03 +0900

    Update minor refactor: removed uncessary code

--- findKey.js
+++ findKey.js
@@ -2,8 +2,6 @@ const findKey = (obj, callback) => {
   for (const [key, value] of Object.entries(obj)) {
     if (callback(value)) return key;
   }
-
-  return undefined;
 };
 
 module.exports = findKey;
