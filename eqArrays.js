973f49ae568bbf546edd18b6dd9b0ab8b78b14f7
Billy Wong <bwong.yh@protonmail.com>
2022-04-15 16:14:29 +0900

    Update minor refactor: arrays are sorted before comparsion

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,17 @@
 const eqArrays = (arr1, arr2) => {
-  let result = true;
-
   if (arr1.length !== arr2.length) return false;
 
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
+  const array1 = arr1.sort((a, b) => {
+    if (a < b) return -1;
+  });
+  const array2 = arr2.sort((a, b) => {
+    if (a < b) return -1;
+  });
+
+  let result = true;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
   }
 
   return result;
